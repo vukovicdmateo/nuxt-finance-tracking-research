6622afb3460ae232fbd23504fb4b5382955274a2
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-09-02 15:04:28 +0200

    Implement currency formatting composable

---
+++ app/composables/useCurrency.js
@@ -0,0 +1,13 @@
+export const useCurrency = (amount) => {
+    const currency = computed(() => {
+      return new Intl.NumberFormat('en-IN', {
+        style: 'currency',
+        currency: 'EUR'
+      }).format(isRef(amount) ? amount.value : amount)
+    })
+  
+    return {
+      currency
+    }
+  }
+  
