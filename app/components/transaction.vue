adec13ba8069fef6445641edeb9e07f74bdc9491
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-09-04 15:59:47 +0200

    Fetch data with the Supabase client

--- app/components/transaction.vue
+++ app/components/transaction.vue
@@ -1,41 +1,45 @@
 <template>
-    <div class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100">
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-1">
-          <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
-          <div>Salary</div>
-        </div>
-  
-        <div>
-          <UBadge color="white">Category</UBadge>
-        </div>
+  <div class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-1">
+        <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
+        <div>{{ transaction.description }}</div>
       </div>
-  
-      <div class="flex items-center justify-end space-x-2">
-        <div>{{ currency }}</div>
-        <div>
-          <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-            <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" />
-          </UDropdown>
-        </div>
+
+      <div>
+        <UBadge color="white" v-if="transaction.category">{{ transaction.category }}</UBadge>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  const { currency } = useCurrency(3000)
-  const items = [
-    [
-      {
-        label: 'Edit',
-        icon: 'i-heroicons-pencil-square-20-solid',
-        click: () => console.log('Edit')
-      },
-      {
-        label: 'Delete',
-        icon: 'i-heroicons-trash-20-solid',
-        click: () => console.log('Delete')
-      }
-    ]
-  ]
-  </script>
+
+    <div class="flex items-center justify-end space-x-2">
+      <div>{{ currency }}</div>
+      <div>
+        <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+          <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" />
+        </UDropdown>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+transaction: Object
+});
+
+const { currency } = useCurrency(props.transaction.amount);
+const items = [
+[
+  {
+    label: 'Edit',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    click: () => console.log('Edit')
+  },
+  {
+    label: 'Delete',
+    icon: 'i-heroicons-trash-20-solid',
+    click: () => console.log('Delete')
+  }
+]
+]
+</script>
