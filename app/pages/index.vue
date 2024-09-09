508d4509c423fd2f3f6b45c597be0939d609ced6
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-09-09 14:43:32 +0200

    Implement daily summary of transactions

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,7 +16,10 @@
     </section>
 
     <section>
-        <Transaction v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
+        <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
+        <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
+        <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" />
+    </div>
     </section>
 </template>
 
@@ -36,10 +39,11 @@ const { data } = useAsyncData('transactions', async () => {
     return data
 } );
 
-transactions.value = data.value;
+transactions.value = data.value || [];
 
 const transactionsGroupedByDate = computed(() => {
   let grouped = {}
+  console.log(transactions.value)
   for (const transaction of transactions.value) {
     const date = new Date(transaction.created_at).toISOString().split('T')[0]
     if (!grouped[date]) {
